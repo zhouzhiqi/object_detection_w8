092b877172065ad1ae524cbda860c3c8daf5789a
zhouzhiqi <zhouzhiqi1234@outlook.com>
2018-04-30 11:41:32 +0800

    c

--- run.py
+++ run.py
@@ -1,22 +1,14 @@
 import os
 
-"""
+
 print('current working dir [{0}]'.format(os.getcwd()))
 w_d = os.path.dirname(os.path.abspath(__file__))
+w_d = os.getcwd()
 
-def show_dir(path):
-    for i in os.listdir(path):
-        if os.path.isdir(i):
-            print('dir',os.path.abspath(i))
-            show_dir(i)
-        else:
-            print('fil',os.path.abspath(i))
-            
-show_dir(w_d)
 
-print('change wording dir to [{0}]'.format(w_d))
-os.chdir(w_d)
-"""
+# print('change wording dir to [{0}]'.format(w_d))
+# os.chdir(w_d)
+
 
 """
 def show_dir(path):
