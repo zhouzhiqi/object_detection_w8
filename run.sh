0c31dce481970e8283730b98d95b908a2cba8df5
zhouzhiqi <zhouzhiqi1234@outlook.com>
2018-04-30 14:08:50 +0800

    c

--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 # 查找脚本所在路径，并进入
 #DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 DIR=$PWD
-cd $DIR
+#cd $DIR
 echo current dir is $PWD
 
 # 设置目录，避免module找不到的问题
 export PYTHONPATH=$PYTHONPATH:$DIR:$DIR/slim:$DIR/object_detection
 
 # 定义各目录
-output_dir=/output  # 训练目录
-dataset_dir=/data/zhouzhiqi/quiz-w8-data # 数据集目录，这里是写死的，记得修改
+output_dir=../output  # 训练目录
+dataset_dir=../data/zhouzhiqi/quiz-w8-data # 数据集目录，这里是写死的，记得修改
 
 train_dir=$output_dir/train
 checkpoint_dir=$train_dir
