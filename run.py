07bc884e2a55078118110890cde18f3f0af8b199
zhouzhiqi <zhouzhiqi1234@outlook.com>
2018-04-30 11:24:46 +0800

    查看dir

--- run.py
+++ run.py
@@ -1,10 +1,5 @@
 import os
 
-print('current working dir [{0}]'.format(os.getcwd()))
-w_d = os.path.dirname(os.path.abspath(__file__))
-print('change wording dir to [{0}]'.format(w_d))
-os.chdir(w_d)
-
 def show_dir(path):
     for i in os.listdir(path):
         if os.path.isdir(i):
@@ -14,6 +9,23 @@ def show_dir(path):
             print('fil',os.path.abspath(i))
             
 show_dir(w_d)
+
+print('current working dir [{0}]'.format(os.getcwd()))
+w_d = os.path.dirname(os.path.abspath(__file__))
+print('change wording dir to [{0}]'.format(w_d))
+os.chdir(w_d)
+
+"""
+def show_dir(path):
+    for i in os.listdir(path):
+        if os.path.isdir(i):
+            print('dir',os.path.abspath(i))
+            show_dir(i)
+        else:
+            print('fil',os.path.abspath(i))
+            
+show_dir(w_d)
+"""
 
 for l in os.popen('/bin/bash -c "cd {0} && source ./run.sh"'.format(w_d)):
     print(l.strip())
