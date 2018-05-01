1e6276d6d18c60a0ec2724c4cbc4b3e71c03610b
zhouzhiqi <zhouzhiqi1234@outlook.com>
2018-05-01 08:21:19 +0800

    config-dir

--- run.sh
+++ run.sh
@@ -30,8 +30,8 @@ cp $DIR/$config $pipeline_config_path
 for i in {0..4}  # for循环中的代码执行5此，这里的左右边界都包含，也就是一共训练500个step，每100step验证一次
 do
     echo "############" $i "runnning #################"
-    last=$[$i*100]
-    current=$[($i+1)*100]
+    last=$[$i*10]
+    current=$[($i+1)*10]
     sed -i "s/^  num_steps: $last$/  num_steps: $current/g" $pipeline_config_path  # 通过num_steps控制一次训练最多100step
 
     echo "############" $i "training #################"
